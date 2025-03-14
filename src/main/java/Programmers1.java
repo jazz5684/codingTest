e56c4585456ec46a70f79d7c82b5df4431847617
jazz5684 <rdw77477410@gmail.com>
2025-03-14 15:45:28 +0900

    Initial commit

---
+++ src/main/java/Programmers1.java
@@ -0,0 +1,7 @@
+public class Programmers1 {
+    public static void main(String[] args) {
+        String message = "Let's go!";
+        System.out.println("3\n2\n1");
+        System.out.println(message);
+    }
+}
