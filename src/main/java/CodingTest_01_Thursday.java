6363e71cfb96487eedd36a1e995205a8f0aeaf6c
jazz5684 <rdw77477410@gmail.com>
2025-03-20 14:50:27 +0900

    5번째문제

--- src/main/java/CodingTest_01_Thursday.java
+++ src/main/java/CodingTest_01_Thursday.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class CodingTest_01_Thursday {
     public static void main(String[] args) {
+        //
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         int m = sc.nextInt();
--- src/main/java/CodingTest_03_Tuesday.java
+++ src/main/java/CodingTest_03_Tuesday.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CodingTest_03_Tuesday {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //
         int a = sc.nextInt();
         int b = sc.nextInt();
 
