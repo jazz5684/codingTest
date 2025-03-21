2dbd1893a50307c60a6406acef822c03165b38ba
jazz5684 <rdw77477410@gmail.com>
2025-03-21 15:08:01 +0900

    5번째문제

---
+++ src/main/java/Code02_10.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Code02_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        num = sc.nextInt();
+        System.out.println("사용자가 입력한 값 ==>" + num);
+
+        sc.close();
+
+    }
+}
